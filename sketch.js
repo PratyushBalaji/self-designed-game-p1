cbeb708752e635c81d2e68121385cd38a41dd2a3
PratyushBalaji <59258572+PratyushBalaji@users.noreply.github.com>
2020-04-18 11:56:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+var bg;
+
+var player, ground;
+
+var canvas;
+
+var flag = 1;
+
+function preload(){
+
+  bg = loadImage("images/Backgrounds/grass.png");
+
+}
+
+
+function setup() {
+  canvas = createCanvas(displayWidth-30, displayHeight-30);
+
+  ground = createSprite(displayWidth/2, displayHeight - displayHeight/3, displayWidth, 10);
+
+  player = createSprite(100, ground.y - 30, 50, 50);
+}
+
+
+function draw() {
+  background(bg);
+  player.collide(ground);
+
+
+  if(keyDown(RIGHT_ARROW)){
+    player.x += 10;
+  }
+  if(keyDown(LEFT_ARROW)){
+    player.x -= 10;
+  }
+  // change background from right side
+  if(player.x > canvas.width && flag === 1){
+    player.x = 10;
+    bg = loadImage("images/Backgrounds/desert.png")
+    flag = 2;
+  }
+
+  if(player.x > canvas.width && flag === 2){
+    player.x = 10;
+    bg = loadImage("images/Backgrounds/empty.png")
+    flag = 3;
+  }
+
+  if(player.x > canvas.width && flag === 3){
+    player.x = 10;
+    bg = loadImage("images/Backgrounds/fall.png")
+    flag = 4;
+  }
+  //change background from left side
+  if(player.x < 10 && flag === 4){
+    player.x = canvas.width;
+    bg = loadImage("images/Backgrounds/empty.png")
+    flag = 3;
+  }
+
+  if(player.x < 10 && flag === 3){
+    player.x = canvas.width;
+    bg = loadImage("images/Backgrounds/desert.png")
+    flag = 2;
+  }
+  if(player.x < 10 && flag === 2){
+    player.x = canvas.width;
+    bg = loadImage("images/Backgrounds/grass.png")
+    flag = 1;
+  }
+
+  //stop at end of animated background
+  if(flag === 4){
+    wall = createSprite(canvas.width, displayHeight - displayHeight/3, 10, displayHeight);
+    player.collide(wall);
+    wall.visible = false;
+  }
+  if(flag === 1){
+    start = createSprite(1, displayHeight - displayHeight/3, 10, displayHeight);
+    player.collide(start);
+    start.visible = false;
+  }
+
+  drawSprites();
+}
+
